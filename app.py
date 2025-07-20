2946aacb1ff1424b1690ce1f613d775d193915da
SimonE1003 <u3629738@connect.hku.hk>
2025-07-20 10:56:11 +0800

    ..

--- app.py
+++ app.py
@@ -190,7 +190,6 @@ def today():
 
         today_date = datetime.strptime(date.today().isoformat(), '%Y-%m-%d').date()
         
-        current_day = 21
         if current_day == 21:
             return render_template("today3.html", habits=habits)
         
